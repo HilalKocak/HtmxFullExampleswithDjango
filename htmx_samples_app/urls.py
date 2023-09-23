e5600536f102f527abada953c6347d2a93b5b7f0
Hilal38 <hhilalkocak@gmail.com>
2023-09-23 23:27:58 +0300

    refactored

--- htmx_samples_app/urls.py
+++ htmx_samples_app/urls.py
@@ -4,11 +4,8 @@ from htmx_samples_app.views import show_info_view, ProfileUpdateView
 app_name='htmx_samples_app'
 
 urlpatterns = [
-    # path("contact/<int:id>/", show_info_view, name = "show_info_view"),
-    # path("contact/<int:id>/edit/", click_to_edit, name="click_to_edit"),
-    path('contact/<int:pk>/edit/', ProfileUpdateView.as_view(), name='profile_update'),
-    path('contact/<int:pk>/show_info_view/', show_info_view, name='show_info_view'),
-    path('contact/<int:pk>/edit/show_info_view/', show_info_view, name='show_info_view'),
 
+    path('edit/<int:pk>/', ProfileUpdateView.as_view(), name='profile_update'),
+    path('edit/<int:pk>/show_info_view/', show_info_view, name='show_info_view'),
 
 ]
